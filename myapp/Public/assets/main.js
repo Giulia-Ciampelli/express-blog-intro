f296544f0ccb419bb72f6b9c19329f4778a97b47
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-10-31 15:42:23 +0100

    human code

--- myapp/Public/assets/main.js
+++ myapp/Public/assets/main.js
@@ -5,6 +5,14 @@ axios.get('http://127.0.0.1:3005/posts')
 .then(response => {
     let posts = response.data;
     console.log(posts);
+
+    // ciclo nell'array
+
+    // destrutturazione post
+
+    // creazione markup
+
+    // assegnazione markup
 })
 .catch(err => {
     console.error(err);
