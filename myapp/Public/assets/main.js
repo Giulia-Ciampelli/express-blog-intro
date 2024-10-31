677f9b79b8eda85d7fdc67bf661fc7c1d0e9d0c8
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-10-31 15:49:48 +0100

    add global variants, start foreach loop

--- myapp/Public/assets/main.js
+++ myapp/Public/assets/main.js
@@ -1,19 +1,28 @@
 console.log('script dellindex pubblico');
 
+// #region variabili globali
+let rowElement = document.querySelector('.row');
+let postElements = '';
+// #endregion variabili globali
+
 // chiamata ajax dalla rotta posts
 axios.get('http://127.0.0.1:3005/posts')
-.then(response => {
-    let posts = response.data;
-    console.log(posts);
+    .then(response => {
+        let posts = response.data;
+        console.log(posts);
 
-    // ciclo nell'array
+        // ciclo nell'array
+        posts.forEach(post => {
 
-    // destrutturazione post
+            // destrutturazione post
+            const {content, img, tags} = post;
 
-    // creazione markup
+            // creazione markup
 
-    // assegnazione markup
-})
-.catch(err => {
-    console.error(err);
-})
+        });
+
+        // assegnazione markup
+    })
+    .catch(err => {
+        console.error(err);
+    })
